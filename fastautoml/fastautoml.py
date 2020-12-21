acffcbfc1ef194878c60da945025e1a10bdc96da
R. Garcia Leiva <rgarcialeiva@gmail.com>
2020-12-21 16:43:15 +0100

    Implementation of filters

--- fastautoml/fastautoml.py
+++ fastautoml/fastautoml.py
@@ -24,6 +24,7 @@ from sklearn.utils.validation import check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
 from sklearn.preprocessing    import KBinsDiscretizer
 from sklearn.preprocessing    import LabelEncoder
+from sklearn.preprocessing    import MinMaxScaler
 from sklearn.calibration      import CalibratedClassifierCV
 from sklearn.cluster          import KMeans
 
@@ -1989,6 +1990,8 @@ class Nescience(BaseEstimator):
 
 class AutoClassifier(BaseEstimator, ClassifierMixin):
     
+    # TODO: Class documentation
+    
     def __init__(self, auto=True, random_state=None):
         
         self.random_state = random_state
@@ -3203,3 +3206,126 @@ class AutoTimeSeries(BaseEstimator, RegressorMixin):
             viu       = new_viu
         
         return (nsc, model, viu)
+
+
+class IncompressibleClassifier():
+    
+    # TODO: Class documentation
+    
+    def __init__(self, auto=False, random_state=None):
+
+        # TODO: Document
+        
+        self.auto         = auto
+        self.random_state = random_state
+
+        return None
+
+
+    def fit(self, X, y, model=None):
+
+        self.X_, self.y_ = check_X_y(X, y, dtype=None)
+
+        # TODO: check it is a valid model
+        # TODO: train if the model if auto = True
+
+        self.model = model
+
+        y_hat = self.model.predict(self.X_)
+        self.incompressible = np.where(self.y_ != y_hat)[0]
+
+        return
+
+
+    def fit_classification(self):
+
+        if self.y_isnumeric:
+
+            regressor = AutoRegressor()
+
+        else:
+
+            model = AutoClassifier()
+
+        return
+
+
+    def get_incompressible(self):
+
+        # TODO: Document
+
+        return self.incompressible
+
+
+    def clusters(self, n_clusters="Auto", filter_inertia=True, filter_repeated_attrs=True, filter_balancedness=True, filter_miscoding=True):
+
+        # TODO: Check that the class is fitted
+
+        # TODO: Allow to change the dimension of the cluster
+        # TODO: Implement a filter based on miscoding
+        # TODO: Implement a filter based on balancedness
+
+        nis = len(self.incompressible)
+
+        # Automatically select the number of clusters
+        if n_clusters == "Auto":
+            n_clusters = int(np.log2(nis)/2)
+
+        # TODO : MinMaxScaler() ??
+        km_model = KMeans(n_clusters=n_clusters)
+
+        # Compute all possible clusters
+
+        df = pd.DataFrame(columns = ["Attribute 1", "Attribute 2", "Cluster", "Inertia"])
+
+        for i in np.arange(self.X_.shape[1]-1):
+    
+            for j in np.arange(i+1, self.X_.shape[1]):
+        
+                new_X = self.X_[np.ix_(self.incompressible,[i, j])]
+                # new_X = scaler.fit_transform(new_X)
+        
+                km_model.fit(new_X)
+        
+                tmp_df = pd.DataFrame([{"Attribute 1": i, "Attribute 2": j, "Cluster": km_model, "Inertia": km_model.inertia_}])
+                df = df.append(tmp_df, ignore_index=True)
+
+        # Filter repeated attributes
+
+        if filter_repeated_attrs:
+
+            attr_in_use = list()
+            final_df    = pd.DataFrame(columns = ["Attribute 1", "Attribute 2", "Cluster", "Inertia"])
+
+            for index, row in df.sort_values(by=['Inertia']).iterrows():
+    
+                if (row["Attribute 1"] in attr_in_use) or (row["Attribute 2"] in attr_in_use):
+                   continue
+        
+                attr_in_use.append(row["Attribute 1"])
+                attr_in_use.append(row["Attribute 2"])
+    
+                tmp_df    = pd.DataFrame([{"Attribute 1": row["Attribute 1"], "Attribute 2": row["Attribute 2"], "Cluster": row["Cluster"], "Inertia": row["Inertia"]}])
+                final_df = final_df.append(tmp_df, ignore_index=True)
+
+        # Filter non-balanced clusters
+
+        if filter_balancedness:
+
+            filter_ratio_low   = 0.2
+            filter_ration_high = 0.8
+
+            for index, row in df.sort_values(by=['Inertia']).iterrows():
+
+                # "N Class 0":np.sum(y_pred==0), "N Class 1":np.sum(y_pred==1
+                # df["Ratio"] = df["N Class 0"] / (df["N Class 0"] + df["N Class 1"])
+                # 
+                # if row['Ratio'] < filter_ratio_low:
+                #    continue
+                # 
+                # if row['Ratio'] > filter_ration_high:
+                #     continue
+
+        return final_df
+
+
